1085d16ba15b4d06f4039ae6ed712aa5ae43e176
bo7owers <rene7orres@gmail.com>
2022-08-18 21:57:11 -0400

    Add main Cont

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import Nav from "../Nav/Nav";
+import Main from "../MainCont/MainCont";
 
 function App() {
   return (
     <>
       <Nav />
-      <p>test</p>
+      <Main />
     </>
   );
 }
---
+++ src/components/MainCont/MainCont.jsx
@@ -0,0 +1,28 @@
+import "./MainCont.css";
+
+function Main() {
+  return (
+    <>
+      <main>
+        <h1>
+          Hello there
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam,
+            dolorum. Vero, sequi nemo. Eaque debitis commodi, fugit ex
+            doloremque molestiae. Doloribus illum, optio reprehenderit hic unde
+            reiciendis similique soluta! Iure! Hic assumenda architecto iste,
+            qui, rerum minus sapiente inventore, veritatis explicabo quaerat
+            modi ipsa rem soluta repellat voluptatem deleniti ex dolorem libero
+            tempora quas optio iusto. Quisquam tempora sed officia. Maxime est
+            id, reprehenderit eligendi assumenda sapiente harum, tenetur
+            consectetur architecto quaerat, recusandae facilis in? Ipsa ad aut,
+            at accusantium voluptatibus, voluptatum ducimus tenetur nemo, nisi
+            dignissimos animi soluta quam.
+          </p>
+        </h1>
+      </main>
+    </>
+  );
+}
+
+export default Main;
