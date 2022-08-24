871b09a8e521b5da0612d263830e1df4d88ccff9
bo7owers <rene7orres@gmail.com>
2022-08-24 18:45:56 -0400

    Style searchbar using emotion

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,29 +1,58 @@
-import "./searchbar.css";
-import { useState } from "react";
-
+// import { useState } from "react";
+import { css } from '@emotion/react'
+import styled from '@emotion/styled'
+const searchBgColour = '#339949'
+const Paragraph = styled.p`
+    color: #fff;
+`
 export default function SearchBar(props) {
-  // First var then func
-  // const [userInput, setInput] = useState("");
+    // First var then func
+    // const [userInput, setInput] = useState("");
 
-  const submitted = (e) => {
-    e.preventDefault();
-    // target name of form
-    // setInput(e.target["custom-search"].value);
-    // props comming from App.jsx
+    const submitted = (e) => {
+        e.preventDefault()
+        // target name of form
+        // setInput(e.target["custom-search"].value);
+        // props comming from App.jsx
 
-    props.addTerm(e.target["custom-search"].value);
-  };
+        props.addTerm(e.target['custom-search'].value)
+    }
 
-  return (
-    <section className="search-bar">
-      <h2>Search for something interesting</h2>
-      <form onSubmit={submitted}>
-        <input type="text" name="custom-search" className="search-text" />
-        <button className="btn-primary btn" name="search-btn">
-          Search
-        </button>
-      </form>
-      {props.userInput ? <p>You searched for {props.userInput}</p> : ""}
-    </section>
-  );
+    return (
+        <section
+            css={css`
+                background-color: ${searchBgColour};
+                font-size: 1rem;
+                display: flex;
+                flex-direction: row;
+                justify-content: center;
+                align-items: center;
+                padding: 1rem;
+                /* new */
+                gap: 2rem;
+                z-index: -1;
+                margin-top: -5px;
+                color: #fff;
+            `}
+        >
+            <h2>Search for something interesting</h2>
+            <form onSubmit={submitted}>
+                <input
+                    type='text'
+                    name='custom-search'
+                    className='search-text'
+                />
+                <button className='btn-primary btn' name='search-btn'>
+                    Search
+                </button>
+            </form>
+            <div aria-live='polite'>
+                {props.userInput ? (
+                    <Paragraph>You searched for {props.userInput}</Paragraph>
+                ) : (
+                    ''
+                )}
+            </div>
+        </section>
+    )
 }
