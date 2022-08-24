8e9daa6d513e81738d301474f7dc313b98b0f144
bo7owers <rene7orres@gmail.com>
2022-08-24 18:07:52 -0400

    Configure emotion properly

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,32 +1,44 @@
-import "./Nav.css";
-
+import './Nav.css'
+import { css } from '@emotion/react'
+const color = ' #369'
 function Nav() {
-  return (
-    <>
-      <div className="main-nav">
-        <img src="./vite.svg" alt="" className="logo" />
-        <nav>
-          <ul className="nav-list">
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Nav 1
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Nav 2
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Nav 3
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div
+                className='main-nav'
+                css={css`
+                    padding: 32px;
+                    background-color: hotpink;
+                    font-size: 24px;
+                    border-radius: 4px;
+                    &:hover {
+                        color: ${color};
+                    }
+                `}
+            >
+                <img src='./vite.svg' alt='' className='logo' />
+                <nav>
+                    <ul className='nav-list'>
+                        <li className='nav-item'>
+                            <a className='nav-link' href='#'>
+                                Nav 1
+                            </a>
+                        </li>
+                        <li className='nav-item'>
+                            <a className='nav-link' href='#'>
+                                Nav 2
+                            </a>
+                        </li>
+                        <li className='nav-item'>
+                            <a className='nav-link' href='#'>
+                                Nav 3
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </>
+    )
 }
 
-export default Nav;
+export default Nav
