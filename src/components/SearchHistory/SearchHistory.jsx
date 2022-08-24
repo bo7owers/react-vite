b7e92c41ed810c9db1f365aa57741ff1df8eeec6
bo7owers <rene7orres@gmail.com>
2022-08-24 19:09:55 -0400

    style search history with emotion

--- src/components/SearchHistory/SearchHistory.jsx
+++ src/components/SearchHistory/SearchHistory.jsx
@@ -1,11 +1,31 @@
-import "./search-history.css";
+// import './search-history.css'
+import styled from '@emotion/styled'
+
+const UlBgColour = '#ffefbf'
+const LiFirstColour = '#5c0202'
+
+const Ul = styled.ul`
+    padding: 1rem 2rem;
+    background-color: ${UlBgColour};
+    border-bottom-left-radius: 15px;
+    border-bottom-right-radius: 15px;
+`
+
+const Li = styled.li`
+    margin: 0 2rem;
+    list-style-type: square;
+    &:nth-child(1) {
+        color: ${LiFirstColour};
+    }
+`
+
 export default function SearcHistory(props) {
-  return (
-    // terms come from MainCont
-    <ul className="search-list">
-      {props.terms.map((term) => (
-        <li key={term}>{term}</li>
-      ))}
-    </ul>
-  );
+    return (
+        // terms come from MainCont
+        <Ul className='search-list'>
+            {props.terms.map((term) => (
+                <Li key={term}>{term}</Li>
+            ))}
+        </Ul>
+    )
 }
