aa5f84eab643a426a5d9167d6cdefafc28517511
bo7owers <rene7orres@gmail.com>
2022-08-28 14:29:51 -0400

    Set up router and router links

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,68 +1,141 @@
 import './Nav.css'
 import { css } from '@emotion/react'
+import { Link } from 'react-router-dom'
 
 const bgColor = ' #369'
 function Nav() {
     return (
-        // This is not accessible, make two Nav components
-        // One for sm screens (bottom), one for md >= screens
-        <>
-            <div
+        <div
+            css={css`
+                display: flex;
+                flex-flow: row wrap;
+                justify-content: flex-end;
+                align-items: center;
+                background-color: ${bgColor};
+                height: 6em;
+                padding: 1.5em;
+                position: absolute;
+                bottom: 0;
+                width: 100vw;
+                @media screen and (min-width: 480px) {
+                    position: relative;
+                    top: 0;
+                }
+            `}
+        >
+            <nav
                 css={css`
                     display: flex;
                     flex-flow: row wrap;
                     justify-content: flex-end;
                     align-items: center;
-                    background-color: ${bgColor};
-                    height: 6em;
-                    padding: 1.5em;
-                    position: absolute;
-                    bottom: 0;
-                    width: 100vw;
-                    @media screen and (min-width: 480px) {
-                        position: relative;
-                        top: 0;
-                    }
+                    gap: 1rem;
+                    list-style-type: none;
                 `}
             >
-                <img
-                    src='./vite.svg'
-                    alt=''
+                <ul
                     css={css`
-                        margin-right: auto;
+                        display: flex;
+                        flex-flow: row wrap;
+                        justify-content: flex-end;
+                        align-items: center;
+                        gap: 1rem;
+                        list-style-type: none;
                     `}
-                />
-                <nav>
-                    <ul
-                        css={css`
-                            display: flex;
-                            flex-flow: row wrap;
-                            justify-content: flex-end;
-                            align-items: center;
-                            gap: 1rem;
-                            list-style-type: none;
-                        `}
-                    >
-                        <li className='nav-item'>
-                            <a className='nav-link' href='#'>
-                                Nav 1
-                            </a>
-                        </li>
-                        <li className='nav-item'>
-                            <a className='nav-link' href='#'>
-                                Nav 2
-                            </a>
-                        </li>
-                        <li className='nav-item'>
-                            <a className='nav-link' href='#'>
-                                Nav 3
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </>
+                >
+                    <li>
+                        <Link
+                            to='/films'
+                            css={css`
+                                color: #ffffff;
+                            `}
+                        >
+                            Films
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to='/people'
+                            css={css`
+                                color: #ffffff;
+                            `}
+                        >
+                            People
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to='/planets'
+                            css={css`
+                                color: #ffffff;
+                            `}
+                        >
+                            Planets
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     )
 }
 
 export default Nav
+
+{
+    /* <div
+css={css`
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: flex-end;
+    align-items: center;
+    background-color: ${bgColor};
+    height: 6em;
+    padding: 1.5em;
+    position: absolute;
+    bottom: 0;
+    width: 100vw;
+    @media screen and (min-width: 480px) {
+        position: relative;
+        top: 0;
+    }
+`}
+> */
+}
+{
+    /* <img
+    src='./vite.svg'
+    alt=''
+    css={css`
+        margin-right: auto;
+    `}
+/>
+<nav>
+    <ul
+        css={css`
+            display: flex;
+            flex-flow: row wrap;
+            justify-content: flex-end;
+            align-items: center;
+            gap: 1rem;
+            list-style-type: none;
+        `}
+    >
+        <li className='nav-item'>
+            <a className='nav-link' href='#'>
+                Nav 1
+            </a>
+        </li>
+        <li className='nav-item'>
+            <a className='nav-link' href='#'>
+                Nav 2
+            </a>
+        </li>
+        <li className='nav-item'>
+            <a className='nav-link' href='#'>
+                Nav 3
+            </a>
+        </li>
+    </ul>
+</nav>
+</div> */
+}
