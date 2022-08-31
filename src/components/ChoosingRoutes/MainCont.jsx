ad5d6cc29a9dd4db6cdb670d70208226a4c12151
bo7owers <rene7orres@gmail.com>
2022-08-30 22:45:18 -0400

    Leave todo comments

--- src/components/ChoosingRoutes/MainCont.jsx
+++ src/components/ChoosingRoutes/MainCont.jsx
@@ -19,6 +19,8 @@ export default function MainCont(props) {
             }
         })()
     }, [pathname])
+
+    // TODO: add 404 page
     return (
         <div className='mainContent'>
             <Routes>
